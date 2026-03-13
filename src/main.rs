31304aa2605a7a0f49376319820835e21172160d
Sébastien Bouchard <syfcorp@proton.me>
2026-03-13 15:45:05 -0400

    feat: security newtypes with adversarial tests (TDD)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-mod errors;
+use ama::errors;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
     tracing::info!("AMA P0 starting...");
-    // Config loading, server setup will be added in later tasks
 }
